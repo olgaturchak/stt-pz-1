9ef7d10cfeded4983488c2f18f8ef757799b73b0
Andrii Zadvornyi <azadvornyi@deloitte.com>
2022-09-26 17:33:35 +0300

    (add) add new lib file

---
+++ lib.js
@@ -0,0 +1,59 @@
+function sum(a, b) {
+
+}
+
+function subtraction(a, b) {
+
+}
+
+function division(a, b) {
+
+}
+
+function multiplication() {
+
+}
+
+function percentage() {
+
+}
+
+function getWholeNumberPart() {
+
+}
+
+function getEvenNumbers() {
+
+}
+
+function getMaxNumber() {
+
+}
+
+function getMinAndMaxNumbers() {
+
+}
+
+function arithmeticMeans() {
+
+}
+
+function getVowels() {
+
+}
+
+function cancatString() {
+
+}
+
+function splitString() {
+
+}
+
+function hasSubString() {
+
+}
+
+function isLowerCase() {
+
+}
