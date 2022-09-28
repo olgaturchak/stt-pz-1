9cfe4abc92afdcbb70d624eda932582ba2a1b927
Andrii Zadvornyi <azadvornyi@deloitte.com>
2022-09-28 15:32:17 +0300

    (add) finish description of tacks

---
+++ js/lib.spec.js
@@ -0,0 +1,39 @@
+describe('Test suite for testing lib.js', () => {
+  describe('Test suite for testing sum function', () => {
+    it('should return sum of two numbers', () => {
+      assert.equal(sum(1, 2), 3)
+      assert.equal(sum(-10, 5), -5)
+      assert.equal(sum(-10, 10), 0)
+    });
+
+    it('should return NaN if one parameter is skipped', () => {
+      assert.equal(sum(1), NaN)
+    });
+
+
+    it('should convert string to number if input parameters is string', () => {
+      assert.equal(sum('5', '1'), 6)
+      assert.equal(sum(10, '1'), 11)
+      assert.equal(sum('25', 5), 30)
+    });
+  });
+
+  describe('Test suite for testing pow function', () => {
+    it('should raising x to the n power', () => {
+      // Write your code here
+    });
+
+
+  });
+
+  describe('Test suite for testing removeByName function', () => {
+    it('should remove some element from array', () => {
+      // Write your code here
+    });
+
+    it('should not remove element from array', () => {
+      // Write your code here
+    });
+  });
+});
+
