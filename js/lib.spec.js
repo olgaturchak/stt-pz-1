bd605d1ad54e5aa585ae9c292c29a899a4eb1506
olgaturchak <olgaturchak14@gmail.com>
2025-04-07 20:57:21 +0300

    done

--- js/lib.spec.js
+++ js/lib.spec.js
@@ -1,39 +1,62 @@
 describe('Test suite for testing lib.js', () => {
-  describe('Test suite for testing sum function', () => {
-    it('should return sum of two numbers', () => {
-      assert.equal(sum(1, 2), 3)
-      assert.equal(sum(-10, 5), -5)
-      assert.equal(sum(-10, 10), 0)
+  describe('sum function', () => {
+    it('should return the sum of two numbers', () => {
+      chai.assert.equal(sum(1, 2), 3);
     });
 
-    it('should return NaN if one parameter is skipped', () => {
-      assert.equal(sum(1), NaN)
+    it('should convert strings to numbers', () => {
+      chai.assert.equal(sum('2', '3'), 5);
     });
 
-
-    it('should convert string to number if input parameters is string', () => {
-      assert.equal(sum('5', '1'), 6)
-      assert.equal(sum(10, '1'), 11)
-      assert.equal(sum('25', 5), 30)
+    it('should return NaN for invalid inputs', () => {
+      chai.assert(Number.isNaN(sum(1)));
     });
   });
 
-  describe('Test suite for testing pow function', () => {
-    it('should raising x to the n power', () => {
-      // Write your code here
+  describe('pow function', () => {
+    it('should return power of a number', () => {
+      chai.assert.equal(pow(2, 3), 8);
     });
-
-
   });
 
-  describe('Test suite for testing removeByName function', () => {
-    it('should remove some element from array', () => {
-      // Write your code here
+  describe('factorial function', () => {
+    it('should return factorial of a number', () => {
+      chai.assert.equal(factorial(5), 120);
     });
+  });
 
-    it('should not remove element from array', () => {
-      // Write your code here
+  describe('fibonacci function', () => {
+    it('should return Fibonacci number', () => {
+      chai.assert.equal(fibonacci(5), 5);
+    });
+  });
+
+  describe('removeByName function', () => {
+    it('should remove the name from array', () => {
+      chai.assert.deepEqual(removeByName(['a', 'b', 'c'], 'b'), ['a', 'c']);
+    });
+  });
+
+  describe('makeCounter function', () => {
+    it('should increment the counter', () => {
+      const counter = makeCounter(0);
+      chai.assert.equal(counter(), 0);
+      chai.assert.equal(counter(), 1);
+    });
+  });
+
+  describe('getAsyncTimerId function', () => {
+    it('should return undefined because timerId is set asynchronously', () => {
+      const result = getAsyncTimerId(1000);
+      chai.assert.strictEqual(result, undefined);
+    });
+  });
+
+  describe('asyncMultiply function', () => {
+    it('should return multiplied value after 3 seconds', async function () {
+      this.timeout(4000);
+      const result = await asyncMultiply(5);
+      chai.assert.strictEqual(result, 10);
     });
   });
 });
-
